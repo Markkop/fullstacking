c7e4de6302ac1226c9e326c5469e9544a3d0ee95
Mark Kop <mk.marcelo.mk@gmail.com>
2019-08-29 20:27:20 -0300

    more describers to use for/beforeEach correctly

--- server/api.test.js
+++ server/api.test.js
@@ -3,39 +3,45 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const Product = require('./Product');
 
-describe('GET /', () => {
-  //change this descibre
-  beforeEach(async () => {
-    const url = 'mongodb://127.0.0.1:27017/test';
-    await mongoose.connect(url, {useNewUrlParser: true});
-    delete mongoose.connection.models.Product;
+describe('Server', () => {
+  describe('without acessing MongoDB', () => {
+    afterEach(() => {
+      app.close();
+    });
+
+    it('is successful', done => {
+      request(app)
+        .get('/')
+        .expect(200, done);
+    });
+
+    it('finds our stampler', done => {
+      request(app)
+        .get('/')
+        .expect(/Stampler/)
+        .expect(200, done);
+    });
   });
 
-  afterEach(() => {
-    Product.deleteOne({title: 'skate'}).exec(); //try to run only after the specific test
-    app.close();
-  });
+  describe('acessing MongoDB direcly', () => {
+    afterEach(() => {
+      Product.deleteOne({title: 'skate'}).exec();
+    });
 
-  it('connects', done => {
-    request(app)
-      .get('/')
-      .expect(200, done);
-  });
+    beforeEach(async () => {
+      const url = 'mongodb://127.0.0.1:27017/test';
+      await mongoose.connect(url, {useNewUrlParser: true});
+      delete mongoose.connection.models.Product;
+    });
 
-  it('finds our stampler', done => {
-    request(app)
-      .get('/')
-      .expect(/Stampler/)
-      .expect(200, done);
-  });
-
-  it('creates and finds a skate', done => {
-    const skate = new Product({title: 'skate'});
-    skate.save();
-    request(app)
-      .get('/')
-      .expect('Content-Type', /json/)
-      .expect(/skate/)
-      .expect(200, done);
+    it('creates and finds a skate', done => {
+      const skate = new Product({title: 'skate'});
+      skate.save();
+      request(app)
+        .get('/')
+        .expect('Content-Type', /json/)
+        .expect(/skate/)
+        .expect(200, done);
+    });
   });
 });
