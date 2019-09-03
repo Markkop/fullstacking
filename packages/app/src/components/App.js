e93acbdf092f61a3ae1a8e1141c80a4b7d6e4332
Mark Kop <mk.marcelo.mk@gmail.com>
2019-09-03 19:00:38 -0300

    removed App.js unused dependencies

--- packages/app/src/components/App.js
+++ packages/app/src/components/App.js
@@ -1,8 +1,4 @@
 import React, {Fragment} from 'react';
-import {Text} from 'react-native';
-import graphql from 'babel-plugin-relay/macro';
-import {QueryRenderer} from 'react-relay';
-
 import EventList from './EventList';
 import EventCreate from './EventCreate';
 
