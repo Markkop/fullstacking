128c468a4fad2140773a404338f1c8f0ad08c17d
Mark Kop <mk.marcelo.mk@gmail.com>
2019-09-05 00:25:12 -0300

    removed async, but kept try/catch block

--- packages/server/src/database.js
+++ packages/server/src/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 export async function connectDatabase() {
   try {
     const databaseUrl = "mongodb://127.0.0.1:27017/test";
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true });
+    mongoose.connect(databaseUrl, { useNewUrlParser: true });
     mongoose.connection.once("open", () => {
       console.log(`Connected to database: ${databaseUrl}`);
     });
