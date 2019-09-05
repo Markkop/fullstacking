7f87eb32ab387145d11a88cc8464fa6006750b9e
Mark Kop <mk.marcelo.mk@gmail.com>
2019-09-05 01:34:16 -0300

    added subscription-transport-ws graphql websocket client

--- packages/app/src/relay/Environment.js
+++ packages/app/src/relay/Environment.js
@@ -1,8 +1,19 @@
 import {Environment, Network, RecordSource, Store} from 'relay-runtime';
-
+import {SubscriptionClient} from 'subscriptions-transport-ws';
 import fetchQuery from './fetchQuery';
 
-const network = Network.create(fetchQuery);
+const websocketURL = `ws://localhost:3000/subscriptions`;
+const setupSubscription = (config, variables, cacheConfig, observer) => {
+  const query = config.text;
+  const subscriptionClient = new SubscriptionClient(websocketURL, {
+    reconnect: true,
+  });
+  subscriptionClient.subscribe({query, variables}, (error, result) => {
+    observer.onNext({data: result});
+  });
+};
+
+const network = Network.create(fetchQuery, setupSubscription);
 
 const source = new RecordSource();
 const store = new Store(source);
