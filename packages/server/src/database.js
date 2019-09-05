be68157692174bca8dac014cb956a93865ae5f8c
Mark Kop <mk.marcelo.mk@gmail.com>
2019-09-04 23:09:14 -0300

    database access with async await

--- packages/server/src/database.js
+++ packages/server/src/database.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
-export function connectDatabase() {
-  databaseUrl = "mongodb://127.0.0.1:27017/test";
-  mongoose.connect(databaseUrl, { useNewUrlParser: true });
-  mongoose.connection.once("open", () => {
-    console.log(`Connected to database: ${databaseUrl}`);
-  });
+export async function connectDatabase() {
+  try {
+    const databaseUrl = "mongodb://127.0.0.1:27017/test";
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true });
+    mongoose.connection.once("open", () => {
+      console.log(`Connected to database: ${databaseUrl}`);
+    });
+  } catch (err) {
+    console.log(err.name);
+  }
 }
